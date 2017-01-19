1baf0b92e38861a204b9d0287011c965baa99e58
alfred.kim <alfred.kim@daumkakao.com>
2017-01-19 19:13:13 +0900

    연습문제 3.13 풀이 설명을 위한 주석 추가.

--- src/main/scala/alfred/ch3/Exercies3_13.scala
+++ src/main/scala/alfred/ch3/Exercies3_13.scala
@@ -9,10 +9,32 @@ object Exercies3_13 extends App {
 
   def foldRightWithFoldLeft[A,B](l: List[A], z: B)(f: (A,B) => B): B =
     List.foldLeft(l, (b:B) => b)((g,a) => b => g(f(a,b)))(z)
-
+  // list = List(a0,a1,a2)
+  // g0(=z) : b => b
+  // g1 : b => g0(f(a0,b))
+  // g2 : b => g1(f(a1,b))
+  // g3 : b => g2(f(a2,b))
+  //      b => g1(f(a1,f(a2,b))
+  //      b => g0(f(a0,f(a1,f(a2,b))))
+  //      b => f(a0,f(a1,f(a2,b)))
+  // g3(z)  : f(a0,f(a1,f(a2,z))
 
   def foldLeftWithFoldRight[A,B](l: List[A], z: B)(f: (B,A) => B): B =
     List.foldRight(l, (b:B) => b)((a,g) => b => g(f(b,a)))(z)
+  // list = List(a0,a1,a2)
+  // g0(=z) : b => b
+  // g1 : b => g0(f(b,a2))
+  // g2 : b => g1(f(b,a1))
+  // g3 : b => g2(f(b,a0))
+  //      b => g1(f(f(b,a0),a1))
+  //      b => g0(f(f(f(b,a0),a1),a2))
+  //      b => f(f(f(b,a0),a1),a2)
+  // g3(z)  : f(f(f(z,a0),a1),a2)
+
+
+
+
+
 
   val list = List("a","b","c")
   val res1 = List.foldRight       (list,"")( (a,g) => g + a )
