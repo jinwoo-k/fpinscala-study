a8b5bd08e0de3072c89154db39a493ab867e216f
alfred.kim <alfred.kim@daumkakao.com>
2017-02-14 12:00:55 +0900

    연습문제 5.16 풀이.

--- src/main/scala/alfred/ch5/Exercies5_16.scala
+++ src/main/scala/alfred/ch5/Exercies5_16.scala
@@ -4,6 +4,6 @@ package alfred.ch5
  * Created by alfredkim on 2017. 2. 3..
  */
 object Exercies5_16 extends App {
-  println(Stream(1,2,3).tails)
-  println(Stream(1,2,3).hasSubsequence(Stream(2,3)))
+
+  println(Stream(1,2,3).scanRight(0)(_ + _))
 }
